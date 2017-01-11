N9-Dev/android_device_htc_flounder

flounder: switch to CM extract-utils
Change-Id: I24ef47b8406adfb6cc78234e64fb6e93858d3226

extract-files.sh
@@ -1,51 +1,57 @@
 #!/bin/bash
-export VENDOR=htc
-export DEVICE_VENDOR=htc
-export DEVICE=flounder
-# Check to see if the user passed a folder in to extract from rather than adb pull
-if [ $# -eq 1 ]; then
-COPY_FROM=$1
-test ! -d "$COPY_FROM" && echo error reading dir "$COPY_FROM" && exit 1
-fi
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 set -e
-function extract() {
-for FILE in `egrep -v '(^#|^$)' $1`; do
-echo "Extracting /system/$FILE ..."
-OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-DEST=${PARSING_ARRAY[1]}
-if [ -z $DEST ]; then
-DEST=$FILE
+
+DEVICE=flounder
+VENDOR=htc
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+CM_ROOT="$MY_DIR"/../../..
+
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
 fi
-DIR=`dirname $FILE`
-if [ ! -d $2/$DIR ]; then
-mkdir -p $2/$DIR
-fi
-if [ "$COPY_FROM" = "" ]; then
-# Try destination target first
-if [ -f /system/$DEST ]; then
-adb pull /system/$DEST $2/$DEST
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+  SRC=adb
 else
-# if file does not exist try OEM target
-if [ "$?" != "0" ]; then
-adb pull /system/$FILE $2/$DEST
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
 fi
-fi
-else
-# Try destination target first
-if [ -f $COPY_FROM/$DEST ]; then
-cp $COPY_FROM/$DEST $2/$DEST
-else
-# if file does not exist try OEM target
-if [ "$?" != "0" ]; then
-cp $COPY_FROM/$FILE $2/$DEST
-fi
-fi
-fi
-done
-}
-DEVICE_BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $DEVICE_BASE/*
-# Extract the device specific files
-extract ../../$DEVICE_VENDOR/$DEVICE/device-proprietary-files.txt $DEVICE_BASE
-./setup-makefiles.sh
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+
+extract "$MY_DIR"/device-proprietary-files.txt "$SRC"
+
+"$MY_DIR"/setup-makefiles.sh

AICP/device_htc_flounder

extract-files.sh
@@ -1,51 +1,57 @@
 #!/bin/bash
-export VENDOR=htc
-export DEVICE_VENDOR=htc
-export DEVICE=flounder
-# Check to see if the user passed a folder in to extract from rather than adb pull
-if [ $# -eq 1 ]; then
-COPY_FROM=$1
-test ! -d "$COPY_FROM" && echo error reading dir "$COPY_FROM" && exit 1
-fi
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 set -e
-function extract() {
-for FILE in `egrep -v '(^#|^$)' $1`; do
-echo "Extracting /system/$FILE ..."
-OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-DEST=${PARSING_ARRAY[1]}
-if [ -z $DEST ]; then
-DEST=$FILE
+
+DEVICE=flounder
+VENDOR=htc
+
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+CM_ROOT="$MY_DIR"/../../..
+
+HELPER="$CM_ROOT"/vendor/aicp/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
 fi
-DIR=`dirname $FILE`
-if [ ! -d $2/$DIR ]; then
-mkdir -p $2/$DIR
-fi
-if [ "$COPY_FROM" = "" ]; then
-# Try destination target first
-if [ -f /system/$DEST ]; then
-adb pull /system/$DEST $2/$DEST
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+  SRC=adb
 else
-# if file does not exist try OEM target
-if [ "$?" != "0" ]; then
-adb pull /system/$FILE $2/$DEST
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
 fi
-fi
-else
-# Try destination target first
-if [ -f $COPY_FROM/$DEST ]; then
-cp $COPY_FROM/$DEST $2/$DEST
-else
-# if file does not exist try OEM target
-if [ "$?" != "0" ]; then
-cp $COPY_FROM/$FILE $2/$DEST
-fi
-fi
-fi
-done
-}
-DEVICE_BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $DEVICE_BASE/*
-# Extract the device specific files
-extract ../../$DEVICE_VENDOR/$DEVICE/device-proprietary-files.txt $DEVICE_BASE
-./setup-makefiles.sh
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+
+extract "$MY_DIR"/device-proprietary-files.txt "$SRC"
+
+"$MY_DIR"/setup-makefiles.sh