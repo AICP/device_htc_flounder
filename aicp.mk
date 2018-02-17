78d6a593edbf7c29c2a67b87bd9617322a43a8fe
Hashbang173 <hashbang173@gmail.com>
2018-02-17 17:31:23 -0500

    flounder: Switch back to AOSP camera app
     * Snap doesn't really bring that much benefits
       to a tablet with such bad camera sensors.
    
    Change-Id: I58058df4f3cf1dc29a9723adef4596edcd91d2af

--- aicp.mk
+++ aicp.mk
@@ -4,13 +4,6 @@
 TARGET_SCREEN_HEIGHT := 2048
 TARGET_SCREEN_WIDTH := 1536
 
-# Camera
-PRODUCT_PACKAGES += \
-    Snap
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.cpp.duplication=false
-
 # Inherit some common AICP stuff
 $(call inherit-product, vendor/aicp/config/common_full_tablet_wifionly.mk)
 
